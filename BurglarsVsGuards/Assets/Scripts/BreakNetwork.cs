796a56da1cb23eb76ffe4f23f51a7e0f0f2f4a1d
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2014-02-15 02:39:50 +0100

    Stuff

--- BurglarsVsGuards/Assets/Scripts/BreakNetwork.cs
+++ BurglarsVsGuards/Assets/Scripts/BreakNetwork.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class BreakNetwork {
 
-
+    private static string name;
     private static bool _server = false;
     public static bool server { get { return _server;}
         set { 
             _server = value;
             if (_server) { // Start server
+                name = Random.value.ToString();
                 Network.InitializeServer(8, 3428, !Network.HavePublicAddress());
-                MasterServer.RegisterHost("Break In", Random.value.ToString());
+                MasterServer.RegisterHost("Break In NGJ2014", name);
             } else { // Stop server
                 Network.Disconnect();
             }
@@ -18,10 +19,15 @@ public class BreakNetwork {
     }
 
     public static bool ConnectRandom() {
-        MasterServer.RequestHostList("Break In");
+
         HostData[] hosts = MasterServer.PollHostList();
         if(hosts.Length == 0) return false;
         Network.Connect(hosts[Mathf.FloorToInt(Random.value * hosts.Length)]);
         return true;
+    }
+
+    public static void Disconnect() {
+        if (_server) MasterServer.UnregisterHost();
+        Network.Disconnect();
     }
 }
--- BurglarsVsGuards/Assets/Scripts/Run.cs
+++ BurglarsVsGuards/Assets/Scripts/Run.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Run : MonoBehaviour {
 
-	void Start () {
-        Debug.Log("Starting Game...");
-        if (!BreakNetwork.ConnectRandom()) {
-            BreakNetwork.server = true;
-            Debug.Log("Starting Server...");
-        } else {
-            Debug.Log("Connecting...");
-        }
+    void Start() {
+        MasterServer.RequestHostList("Break In NGJ2014");
 	}
+
+    void OnGUI() {
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("Host")) {
+            Network.InitializeServer(8, 3428, !Network.HavePublicAddress());
+            MasterServer.RegisterHost("Break In NGJ2014", name);
+        }
+        HostData[] data = MasterServer.PollHostList();
+
+	    foreach(HostData element in data)
+	    {
+		    GUILayout.BeginHorizontal();	
+		    string name = element.gameName + " " + element.connectedPlayers + " / " + element.playerLimit;
+		    GUILayout.Label(name);	
+		    GUILayout.Space(5);
+		    string hostInfo = "[";
+		    foreach (string host in element.ip)
+			    hostInfo = hostInfo + host + ":" + element.port + " ";
+		    hostInfo = hostInfo + "]";
+		    GUILayout.Label(hostInfo);	
+		    GUILayout.Space(5);
+		    GUILayout.Label(element.comment);
+		    GUILayout.Space(5);
+		    GUILayout.FlexibleSpace();
+		    if (GUILayout.Button("Connect"))
+		    {
+			    Network.Connect(element);			
+		    }
+		    GUILayout.EndHorizontal();	
+	    }
+        GUILayout.EndVertical();
+    }
 }
