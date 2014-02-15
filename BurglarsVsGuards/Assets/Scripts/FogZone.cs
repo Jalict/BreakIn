216cfa39ba0bd2df9960be551b834dfcd2bd7b44
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2014-02-15 19:43:54 +0100

    Fog Zone of War Zones

---
+++ BurglarsVsGuards/Assets/Scripts/FogZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject p in GameObject.FindGameObjectsWithTag("Player"))
+            if (collider.bounds.Contains(p.transform.position))
+                renderer.enabled = true;
+	}
+}
