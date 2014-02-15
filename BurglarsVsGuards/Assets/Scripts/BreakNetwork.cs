7c05bfaa7100e6476089bc3fe1061abee0803b86
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2014-02-15 02:10:58 +0100

    Network Connection Test

---
+++ BurglarsVsGuards/Assets/Scripts/BreakNetwork.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakNetwork {
+
+    private static bool _server = false;
+    public static bool server { get { return _server;}
+        set { 
+            _server = value;
+            if (_server) { // Start server
+                Network.InitializeServer(8, 3428, !Network.HavePublicAddress());
+                MasterServer.RegisterHost("Break In", Random.value.ToString());
+            } else { // Stop server
+                Network.Disconnect();
+            }
+        }
+    }
+
+    public static bool ConnectRandom() {
+        HostData[] hosts = MasterServer.PollHostList();
+        if(hosts.Length == 0) return false;
+        Network.Connect(hosts[Mathf.FloorToInt(Random.value * hosts.Length)]);
+        return true;
+    }
+}
---
+++ BurglarsVsGuards/Assets/Scripts/Run.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Run : MonoBehaviour {
+
+	void Start () {
+        if (!BreakNetwork.ConnectRandom())
+            BreakNetwork.server = true;
+	}
+}
