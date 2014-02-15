1cbfec98ee6d44b1a2c76bcd5ed7dbe458c049b8
Wikzo <Linkthewindwaker@gmail.com>
2014-02-15 01:16:51 +0100

    simple mover cube

---
+++ BurglarsVsGuards/Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public float Speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        Vector3 MoveVector = new Vector3(OuyaInput.GetAxis(OuyaAxis.LX, OuyaPlayer.P01),
+            OuyaInput.GetAxis(OuyaAxis.LY, OuyaPlayer.P01),
+            0);
+
+        transform.Translate(MoveVector * Speed * Time.deltaTime);
+	}
+}
