e4ef64e3e6d25ac98e99455729e7345dff653889
Wikzo <Linkthewindwaker@gmail.com>
2014-02-15 14:42:12 +0100

    props pickup started working

---
+++ BurglarsVsGuards/Assets/Scripts/PropsToPickUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropsToPickUp : MonoBehaviour
+{
+
+    public bool CanBePickedUp = true;
+
+
+    public float PickupTime = 3f;
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
