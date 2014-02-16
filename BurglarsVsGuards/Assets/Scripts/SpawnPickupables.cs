bc7755226a560bd2d95289a728a40dda15bdc2f2
Wikzo <Linkthewindwaker@gmail.com>
2014-02-16 08:41:13 +0100

    spawner system now spawns same amount of stuff every time

--- BurglarsVsGuards/Assets/Scripts/SpawnPickupables.cs
+++ BurglarsVsGuards/Assets/Scripts/SpawnPickupables.cs
@@ -5,20 +5,42 @@ public class SpawnPickupables : MonoBehaviour
 {
 
     public GameObject[] SpawnPoints;
-    public GameObject[] Pickupables;
+    public GameObject[] DifferentTypesOfPickups;
+    public int[] NumberOfEach;
+
+    private int divider;
 
     // Use this for initialization
     private void Start()
     {
+        if (DifferentTypesOfPickups.Length != NumberOfEach.Length)
+            Debug.Log("ERROR - DifferentTypesOfPickups and NumberOfEach has to be same size!");
+
+        divider = SpawnPoints.Length/DifferentTypesOfPickups.Length;
+
+        for (int i = 0; i < DifferentTypesOfPickups.Length; i++)
+        {
+            NumberOfEach[i] = divider;
+        }
+
         for (int i = 0; i < SpawnPoints.Length; i++)
         {
-            int random = Random.Range(0, Pickupables.Length-1);
+            int random = Random.Range(0, DifferentTypesOfPickups.Length);
+            int counter = 0;
 
-            string path = "Prefabs/" + Pickupables[random].name;
+            while (NumberOfEach[random] <= 0 && counter < 500)
+            {
+                random = Random.Range(0, DifferentTypesOfPickups.Length);
+                counter++;
+               
+            }
+
+            string path = "Prefabs/" + DifferentTypesOfPickups[random].name;
+            NumberOfEach[random]--;
 
             if (Network.connections.Length > 0)
             {
-                Connector.AddEntity(Pickupables[random].name, SpawnPoints[i].transform.position, Quaternion.identity, path,
+                Connector.AddEntity(DifferentTypesOfPickups[random].name, SpawnPoints[i].transform.position, Quaternion.identity, path,
                                     "Untagged",
                                     true);
             }
